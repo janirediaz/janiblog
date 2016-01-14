a5f4e03a0854763935ad1b9e6736767388a2668c
JanireDiaz <janire.diaz@gmail.com>
2016-01-14 22:35:43 +0100

    moderated comments

--- grails-app/controllers/janiblog/VisitorController.groovy
+++ grails-app/controllers/janiblog/VisitorController.groovy
@@ -22,6 +22,7 @@ class VisitorController {
         Post p = Post.get(params.id);
         Comment c = new Comment();
         c.contenido = params.descripcion;
+        c.moderated = true;
         p.addToComentarios(c);
         if(!p.save(flush:true)){
             println(p.errors.allErrors.join('\n'));
--- grails-app/domain/janiblog/Comment.groovy
+++ grails-app/domain/janiblog/Comment.groovy
@@ -2,9 +2,12 @@ package janiblog
 
 class Comment {
 
+    boolean moderated;
     String contenido;
     static belongsTo = [post:Post];
 
+
     static constraints = {
+
     }
 }
